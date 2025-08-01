2a213b59691668fe5f9834e00efc25bcdfc712e5
Daniel Mezker <dmezker06@gmail.com>
2025-08-01 11:25:01 +0200

    Neu hinzugefügt

--- 01_Simple_CI_Project/01_Simple_CI_Project/Program.cs
+++ 01_Simple_CI_Project/01_Simple_CI_Project/Program.cs
@@ -1,15 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _01_Simple_CI_Project
+class Program
 {
-    internal class Program
+    static int Add(int a, int b) => a + b;
+
+    static void Main()
     {
-        static void Main(string[] args)
+        if (Add(2, 3) != 5)
         {
+            throw new Exception("Test fehlgeschlagen: Add(2, 3) sollte 5 ergeben.");
         }
+
+        Console.WriteLine("Test bestanden. Ergebnis: " + Add(2, 3));
     }
 }
+
