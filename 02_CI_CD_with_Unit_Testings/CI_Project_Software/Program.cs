f80a82006f01a2d6338fdeb14e61b545d3c53452
Daniel Mezker <dmezker06@gmail.com>
2025-08-04 11:42:16 +0200

    Upload

---
+++ 02_CI_CD_with_Unit_Testings/CI_Project_Software/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CI_Project_Software
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
